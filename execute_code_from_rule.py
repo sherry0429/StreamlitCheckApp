159a0e01b782a703174e92881410af92fe877354
yyds1233 <2755824226@qq.com>
2023-06-07 14:39:53 +0800

    读取规则组件的表，满足条件并执行其中代码

---
+++ execute_code_from_rule.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+# 数据库连接参数
+conn_params = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': '7PIug1Lk3O',
+    'host': 'pg-postgresql',
+    'port': '5432',
+    'sslmode': 'disable',
+}
+
+# 遍历搜索并执行代码块
+def execute_code_blocks():
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
+
+    # 查询满足条件的记录
+    select_query = '''
+    SELECT code_block
+    FROM t_check_role_code
+    WHERE enable = TRUE AND start_time <= NOW() AND end_time >= NOW();
+    '''
+    cur.execute(select_query)
+    rows = cur.fetchall()
+
+    # 执行代码块
+    for row in rows:
+        code_block = row[0]
+        exec(code_block)
+
+    cur.close()
+    conn.close()
+
+# 执行搜索和执行代码块的操作
+execute_code_blocks()
