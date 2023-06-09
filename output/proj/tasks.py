212992f51d89ca5103f4a06281b72dd479a9d144
lixiran <1965963533@qq.com>
2023-06-09 02:24:06 +0000

    modified:   output/proj/func.py 	modified:   output/proj/tasks.py 	modified:   output/start.sh

--- output/proj/tasks.py
+++ output/proj/tasks.py
@@ -4,12 +4,11 @@ from celery.schedules import crontab
 app = Celery('proj', include=['proj.func'])
 app.config_from_object('proj.celery_config')
 
-# 定时任务
+# 每隔一分钟写入一次
 app.conf.beat_schedule = {
     "scheduled_push": {
         "task": "proj.func.push",
-        "schedule": 10
+        "schedule": crontab(minute="*/1")
     }
 }
 
-# crontab(minute="*/1")
