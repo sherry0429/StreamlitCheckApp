bea0ca16101a78fa1f9ab71905f273aabb482f67
woliekailea <1152937220@qq.com>
2023-06-09 08:36:38 +0000

    modified:   liekailea/streamlit.py

--- liekailea/streamlit.py
+++ liekailea/streamlit.py
@@ -1,8 +1,25 @@
 import streamlit as st
 import pandas as pd
 import psycopg2
+
+from datetime import datetime
 # psql -h localhost -p 5432 -U postgres
 
+current_role_id = None
+
+def login():
+    st.title("用户登录")
+
+    role_id = st.text_input("角色 ID")
+    password = st.text_input("密码", type="password")
+
+    if st.button("登录"):
+        if password == "password":
+            st.success("登录成功！")
+            return role_id
+        else:
+            st.error("密码错误！")
+            return None
 
 
 # 连接到PG数据库
@@ -23,73 +40,17 @@ def connect_to_pg():
     connection = psycopg2.connect(**db_config)
     return connection
 
-# Streamlit应用程序
-#def main():
-    # st.title("监测结果查询")
-    
-
-    
-    # col1, col2, col3, col4, col5=st.columns([0.2, 1, 0.2, 1, 0.2])
-    # with col1:
-    #     st.empty()
-    # with col2:
-    #     date = st.text_input("请输入日期（YYYY-MM-DD）：")
-    # with col3:
-    #     st.empty()
-    # with col4:
-    #     monitoring_type = st.text_input("请输入监测类型：")
-    # with col5:
-    #     st.empty()
-
-    # date = st.text_input("请输入日期（YYYY-MM-DD）：")
-    # monitoring_type = st.text_input("请输入监测类型：")
-    # if st.button("查询"):
-    # # 查询监测结果
-    #     results = query_results(date, monitoring_type)
-    # # 显示查询结果
-    # for result in results:
-    # result = (date,monitoring_type)
-    # st.write(result)
-
-# def main():
-#     st.sidebar.title("导航")
-#     choice = st.sidebar.radio("选择一个页面", ("模块导入", "模块管理"))
-
-#     if choice == "模块导入":
-#         module_import()
-#     elif choice == "模块管理":
-#         module_management()
-
-# def module_import():
-#     st.title("模块导入")
-
-#     # 输入代码块的输入框
-#     code_input = st.text_area("输入代码块", height=200)
-
-#     # 导入按钮
-#     if st.button("导入"):
-#         try:
-#             exec(code_input)
-#             st.success("模块已成功导入")
-#         except Exception as e:
-#             st.error(f"模块导入出错: {e}")
-
-# def module_management():
-#     st.title("模块管理")
-
-#     # 显示模块的开关
-#     st.sidebar.subheader("模块开关")
-#     module_toggle = st.sidebar.checkbox("显示模块")
-
-#     # 模块详细信息
-#     if module_toggle:
-#         st.subheader("模块详细信息")
-#         st.write("这里是模块的详细信息")
 
 
 
 def main():
-    
+
+    global current_role_id  # 声明全局变量
+    current_role_id = login()  # 保存角色 ID
+
+    if current_role_id is None:
+        return
+
     st.sidebar.title("导航")
     choice = st.sidebar.radio("选择一个页面", ("模块导入", "模块管理"))
 
@@ -98,37 +59,70 @@ def main():
     elif choice == "模块管理":
         page_management()
     
-    # st.set_page_config(page_title="模块导入与管理", page_icon=":books:")
-
-    # st.title("模块导入与管理")
-
-    # menu = ["模块导入", "模块管理"]
-    # choice = st.sidebar.selectbox("选择一个选项", menu)
-
-    # if choice == "模块导入":
-    #     page_import()
-    # else:
-    #     page_management()
 
 def page_import():
     st.header("模块导入")
 
     code = st.text_area("输入代码", height=200)
     if st.button("导入"):
-        print ("1")
-        # 在这里实现导入代码的代码
+        now = datetime.now()  # 获取当前时间
+        conn = connect_to_pg()  # 连接到 PostgreSQL 数据库
+        cur = conn.cursor()
+        cur.execute("INSERT INTO t_check_role_code (role_id, code_block, create_time, start_time, end_time, enable) VALUES (%s, %s, %s, %s, NULL, %s);", (current_role_id, code, now,now,True))  # 将输入的代码插入到 t_check_role_code 表格中
+        conn.commit()  # 提交事务，保存对数据库的修改
+        st.success("代码导入成功！")
 
-def page_management():
+
+
+
+def page_management(role_id):
     st.header("模块管理")
 
-    # 在这里获取已经存在的代码块的启用情况与详细信息，存储在一个Pandas DataFrame中
-    data = pd.DataFrame({
-        "名称": ["代码块1", "代码块2", "代码块3"],
-        "启用": [True, False, True],
-        "详细信息": ["这是代码块1的详细信息", "这是代码块2的详细信息", "这是代码块3的详细信息"]
-    })
+    conn = connect_to_pg()  # 连接到 PostgreSQL 数据库
+    cur = conn.cursor()
 
-    st.dataframe(data)
+    # 查询当前用户可以管理的代码块
+    cur.execute("""
+        SELECT id, code_block, create_time, start_time, end_time, enable
+        FROM t_check_role_code;
+    """)
+    rows = cur.fetchall()
+
+    # 显示代码块列表
+    for row in rows:
+        code_id, code_block, create_time, start_time, end_time, enable = row
+
+        with st.beta_expander(f"代码块 {code_id}"):
+            # 显示代码块内容
+            st.code(code_block)
+
+            # 显示代码块的 ID、创建时间、启用时间和终止时间
+            st.write(f"ID：{code_id}")
+            st.write(f"创建时间：{create_time}")
+            st.write(f"启用时间：{start_time}")
+            st.write(f"终止时间：{end_time}")
+
+            # 允许用户重新启用或关闭代码块
+            if enable:
+                if st.button("关闭"):
+                    cur.execute("""
+                        UPDATE t_check_role_code
+                        SET end_time = %s, enable = FALSE
+                        WHERE id = %s;
+                    """, (datetime.now(), code_id))
+                    conn.commit()  # 提交事务，保存对数据库的修改
+                    st.success("关闭成功！")
+            else:
+                if st.button("启用"):
+                    cur.execute("""
+                        UPDATE t_check_role_code
+                        SET start_time = %s, enable = TRUE
+                        WHERE id = %s;
+                    """, (datetime.now(), code_id))
+                    conn.commit()  # 提交事务，保存对数据库的修改
+                    st.success("启用成功！")
+
+    conn.close()  # 关闭数据库连接
 
 
     
